0d6083f68438a9acc757976988e520e5a43f80f7
Araf Al-Jami <clown1331@yahoo.com>
2017-09-05 05:45:23 +0600

    Name, bootstrap

--- php/user.php
+++ php/user.php
@@ -1,5 +1,7 @@
 <?php 
 	class User{
+		public $fName;
+		public $lName;
 		public $cfName;
 		public $tcName;
 		public $cfRating;
@@ -8,7 +10,9 @@
 		public $tcColor;
 		public $points;
 		
-		public function __construct( $cfName, $tcName ) {
+		public function __construct( $fName, $lName, $cfName, $tcName ) {
+			$this->fName = $fName;
+			$this->lName = $lName;
 			$this->cfName = $cfName;
 			$this->tcName = $tcName;
 		}
